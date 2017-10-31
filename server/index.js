2678e7a52c6bac9207a3b5ce0cb30284058af25d
Daniel <dktdaniel@gmail.com>
2017-10-31 15:54:22 -0700

    Added AWS SQS and routing for data generation

--- server/index.js
+++ server/index.js
@@ -1,96 +1,68 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const faker = require('faker');
 const db = require('../db/index.js');
 // Uncomment below to test database
 // const db = require('../db/test.js');
-const _ = require('lodash');
-const moment = require('moment');
-// const elasticsearch = require('elasticsearch');
-// const client = new elasticsearch.Client({
-//   host: 'localhost:9200',
-//   log: 'trace'
-// });
+
+// ***** AWS *****
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+
+// Load credentials and set the region from the JSON file
+AWS.config.loadFromPath('./config.json');
+
+// SQS service object
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const QueueUrl = "https://sqs.us-west-1.amazonaws.com/810323078514/oblako-analytics";
+// ***************
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//serve up static html
+// Serve up static html
 app.use(express.static(__dirname + '/../client'));
 
-const categories = [
-  'Amazon Device Accessories',
-  'Amazon Kindle',
-  'Automotive & Powersports',
-  'Baby Products (Excluding Apparel)',
-  'Beauty',
-  'Books',
-  'Business Products (B2B)',
-  'Camera & Photo',
-  'Clothing & Accessories',
-  'Collectible Coins',
-  'Electronics (Accessories)',
-  'Fine Art',
-  'Grocery & Gourmet Food',
-  'Handmade',
-  'Health & Personal Care',
-  'Historical & Advertising Collectibles',
-  'Home & Garden',
-  'Industrial & Scientific',
-  'Jewelry',
-  'Luggage & Travel Accessories',
-  'Music',
-  'Musical Instruments',
-  'Sports Collectibles',
-  'Tools & Home Improvement',
-  'Personal Computers',
-  'Professional Services',
-  'Shoes, Handbags & Sunglasses',
-  'Software & Computer Games',
-  'Sports',
-  'Sports Collectibles',
-  'Tools & Home Improvement',
-  'Toys & Games',
-  'Video, DVD & Blu-Ray',
-  'Video Games & Video Game Consoles',
-  'Watches'
-];
+// Routing for data generation
+var dataGeneration = require('./routes/data-generation');
+app.use('/dataGeneration', dataGeneration);
 
-//Add Amazon's main categories and random fraud risk
-app.get('/categories', (req, res) => 
-  Promise.all(categories.map((category, index) => db.addNewCategory(category, index, Math.floor(Math.random() * 100))))
-  .then(success => res.send(success))
-  .catch(err => console.log(err))
-);
+app.get('/sendmessage', (req, res) => {
+  // Sending a message
+  var params = {
+   MessageBody: "This is a test message. Hello!!!",
+   QueueUrl: QueueUrl
+  };
 
-//Generate 1k random devices
-app.get('/devices', (req, res) => {
-  let deviceList = ['nexus', 'iphone', 'ipad', 'motorola'];
-  let osList = ['android', 'ios', 'windows'];
-  const promisesArray = [];
+  sqs.sendMessage(params).promise()
+  .then(data => console.log("Success", data.MessageId))
+  .then(x => res.send('success'))
+  .catch(err => console.log(err));
+})
 
-  _.times(1000, x => {
-    let device = {};
-    device.user_id = Math.floor(Math.random() * 10000);
-    device.device_name = deviceList[Math.floor(Math.random() * deviceList.length)];
-    device.device_os = osList[Math.floor(Math.random() * osList.length)];
-    device.logged_in_at = moment(faker.date.between('2017-07-25', '2017-10-25')).format("YYYY-MM-DD HH:mm:ss");
+app.get('/receiveMessage', (req, res) => {
+  // Sending a message
+  var params = {
+   QueueUrl: QueueUrl
+  };
 
-    promisesArray.push(
-      db.addNewDevice(
-        device.user_id, 
-        device.device_name, 
-        device.device_os, 
-        device.logged_in_at
-      )
-    );
-  });
+  return sqs.receiveMessage(params).promise()
+  .then(data => {
+    console.log(data);
+    var deleteParams = {
+      QueueUrl: QueueUrl,
+      ReceiptHandle: data.Messages[0].ReceiptHandle
+    };
 
-  return Promise.all(promisesArray)
-  .then(success => res.send(success))
-  .catch(err => console.log(err))
-});
+    return sqs.deleteMessage(deleteParams).promise()
+  })
+  .then(x => {
+    console.log('xxxxxx', x);
+    res.send('message received and deleted from queue!');
+  })
+  .catch(err => console.log(err));
+})
+
 
 app.post('/fraud', (req, res) => {
   //Algorithm parameters
@@ -147,130 +119,6 @@ app.post('/fraud', (req, res) => {
     db.updateFraudScore(global_user_id, fraud_score);
     res.send(`order id: ${order_id} total fraud risk ${fraud_score}`)
   })
-  .catch(err => console.log(err))
-});
-
-app.get('/orders', (req, res) => {
-  const generations = 10000;
-
-  //Generate 99% legit orders
-  const legitOrdersGenerated = [];
-  _.times(generations * 0.99, x => {
-    let randomState = faker.address.stateAbbr();
-    let randomZip = faker.address.zipCode();
-    let orderObj = {
-      order: {
-        user_id: Math.floor(Math.random() * 10000), 
-        billing_state: randomState,
-        billing_zip: randomZip,
-        billing_country: 'USA',
-        shipping_state: randomState,
-        shipping_zip: randomZip,
-        shipping_country: 'USA',
-        total_price: faker.commerce.price(),
-        purchased_at: moment(faker.date.between('2017-07-25', '2017-10-25')).utc().format("YYYY-MM-DD HH:mm:ss"),
-        std_dev_from_aov: Math.floor(Math.random() * 2)
-      },
-      // items: [
-      //   {
-      //     item_id: 123456789012,
-      //     quantity: 3
-      //   },
-      //   {
-      //     item_id: 123456789011,
-      //     quantity: 2
-      //   }
-      // ]
-    };
-
-    let o = orderObj.order;
-    return db.addNewOrder(
-      o.user_id, 
-      o.billing_state, 
-      o.billing_zip,
-      o.billing_country,
-      o.shipping_state, 
-      o.shipping_zip,
-      o.shipping_country,
-      o.total_price,
-      o.purchased_at,
-      o.std_devs_from_aov
-    )
-    .then(({insertId}) => {
-      // Generate 2 items in order
-      const itemsGenerated = [];
-        _.times(2, x => {
-          let item = {
-            category_id: Math.floor(Math.random() * categories.length),
-            order_id: insertId,
-            quantity: Math.floor(Math.random() * 4),
-          };
-          itemsGenerated.push(db.addNewItemFromOrder(item.category_id, item.order_id, item.quantity));
-        })
-      return Promise.all(itemsGenerated)
-    })
-  })
-  return Promise.all(legitOrdersGenerated)
-  .then(x => {
-    const fraudOrdersGenerated = [];
-    _.times(generations * 0.01, x => {
-      let orderObj = {
-        order: {
-          user_id: Math.floor(Math.random() * 10000), 
-          billing_state: faker.address.stateAbbr(),
-          billing_zip: faker.address.zipCode(),
-          billing_country: 'USA',
-          shipping_state: faker.address.stateAbbr(),
-          shipping_zip: faker.address.zipCode(),
-          shipping_country: 'USA',
-          total_price: faker.commerce.price(),
-          purchased_at: moment(faker.date.between('2017-07-25', '2017-10-25')).utc().format("YYYY-MM-DD HH:mm:ss"),
-          std_dev_from_aov: Math.floor(Math.random() * 8)
-        },
-        // items: [
-        //   {
-        //     item_id: 123456789012,
-        //     quantity: 3
-        //   },
-        //   {
-        //     item_id: 123456789011,
-        //     quantity: 2
-        //   }
-        // ]
-      };
-
-      let o = orderObj.order;
-      return db.addNewOrder(
-        o.user_id, 
-        o.billing_state, 
-        o.billing_zip,
-        o.billing_country,
-        o.shipping_state, 
-        o.shipping_zip,
-        o.shipping_country,
-        o.total_price,
-        o.purchased_at,
-        o.std_devs_from_aov
-        )
-      .then(({insertId}) => {
-        // Generate 2 items in order
-        const itemsGenerated = [];
-          _.times(2, x => {
-            _.times(2, x => {
-              let item = {
-                category_id: Math.floor(Math.random() * categories.length),
-                order_id: insertId,
-                quantity: Math.floor(Math.random() * 4),
-              };
-              itemsGenerated.push(db.addNewItemFromOrder(item.category_id, item.order_id, item.quantity));
-            })
-          })
-        return Promise.all(itemsGenerated)
-      })
-    })
-  return Promise.all(fraudOrdersGenerated)
-  })
-  .then(success => res.send(success))
   .catch(err => console.log(err))
 });
 
