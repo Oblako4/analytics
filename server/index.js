a21d093628c82046f96d2a380ecdd83dbd1ebcc7
Daniel <dktdaniel@gmail.com>
2017-10-31 17:09:27 -0700

    Refactored fraud alg using async await

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const QueueUrl = require ('../config.js');
 // Uncomment below to test database
 // const db = require('../db/test.js');
 
-// ***** AWS *****
+// ================== AWS ====================
 // Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
 
@@ -15,7 +15,7 @@ AWS.config.loadFromPath('./config.json');
 
 // SQS service object
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-// ***************
+// ===========================================
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -120,6 +120,59 @@ app.post('/fraud', (req, res) => {
     res.send(`order id: ${order_id} total fraud risk ${fraud_score}`)
   })
   .catch(err => console.log(err))
+});
+
+
+app.post('/fraudAsync', async (req, res) => {
+  try {
+    //Algorithm parameters
+    const algWeight = 25;
+    const acceptableAOVStdDev = 3;
+    const acceptableNumOfDevices = 6;
+
+    let fraud_score = 0;
+    let order_id = req.body.order.order_id;
+    // const items = req.body.items;
+
+    //Search for order by order ID
+
+    let orderInfo = await db.searchOrders(order_id)
+    //Grab only the first result
+    //*** INFO FROM ORDERS ***
+    let { billing_name, shipping_name, billing_state, shipping_state, user_id, std_devs_from_aov } = orderInfo[0];
+    fraud_score += billing_state === shipping_state ? 0 : algWeight;
+    //Check if order total is unusually high
+    fraud_score += std_devs_from_aov < acceptableAOVStdDev ? 0 : algWeight;
+
+    //*** INFO FROM USER ***
+    //Search for a user's devices
+    let deviceResult = await db.searchDevices(user_id);
+    // determine if # of devices is high
+    fraud_score += deviceResult.length < acceptableNumOfDevices ? 0 : algWeight;
+
+    //*** INFO FROM INVENTORY ***
+    //Determine if order has items from high-risk categories
+
+      //Get category id for each item in order
+      // Promise.all(items.map(({item_id}) => db.searchItems(item_id))))
+
+      //Until we receive items as part of order info...
+      let itemsFromOrder = await db.getItemsFromOrder(order_id);
+      let categoryIds = itemsFromOrder.map(item => item.category_id);
+      //Get category fraud risk for each item
+      let arrayOfCategoryFraudRisk = await Promise.all(category_ids.map(category_id => db.getCategoryFraudRisk(category_id)));
+
+      //Sum category fraud risk scores
+      let totalCategoriesFraudRisk = arrayOfCategoryFraudRisk.reduce((acc, cur) => acc + cur[0].fraud_risk, 0);
+
+      //Increment fraud score if category risk is over 30
+      fraud_score += totalCategoriesFraudRisk < 80 ? 0 : algWeight; 
+      //Update fraud score for order in database
+      db.updateFraudScore(global_user_id, fraud_score);
+      res.send(`order id: ${order_id} total fraud risk ${fraud_score}`)
+    } catch(e) {
+      await console.log(e);
+    }
 });
 
 app.listen(3000, function() {
