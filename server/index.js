db23e534a2c174b78eeb93f3d78c9d00ac723cfe
Daniel <dktdaniel@gmail.com>
2017-11-06 17:07:28 -0800

    Implemented cache to eliminate unneccessary queries

--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const db = require('../db/index.js');
-const { usersInbox, usersOutbox, ordersInbox, ordersOutbox, inventoryInbox, inventoryOutbox } = require ('../config.js');
 const moment = require('moment');
-// require('nodent')();
-// 'use nodent';
+const { inbox, usersOutbox, ordersOutbox, inventoryOutbox } = require ('../config.js');
+const _ = require('lodash');
 
 // Uncomment below to test database
 // const db = require('../db/test.js');
@@ -17,8 +16,13 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 
 // Routing for data generation
-// const dataGeneration = require('./routes/data-generation');
-// app.use('/dataGeneration', dataGeneration);
+const dataGeneration = require('./routes/data-generation');
+app.use('/dataGeneration', dataGeneration);
+
+
+let redisLite = {
+  // 123456: {haveDevices: false, haveCategories: false},
+}; 
 
 //=========Check if we have all order info in our database===============
 const haveAllOrderInfo = async message => {
@@ -34,62 +38,41 @@ const haveAllOrderInfo = async message => {
         message.user.devices.map(({device_name, device_os, logged_in_at}) => db.addNewDevice(message.user.id, device_name, device_os, moment(logged_in_at).format("YYYY-MM-DD HH:mm:ss")))
       );
 
-      //Check if we have the category
-      let haveItems = await db.searchUserItems(message.user.id);
-      if (haveItems.length > 0) {
+      let order_id_result = await db.getUnprocessedOrder(message.user.id);
+      let order_id = order_id_result[0].id;
+
+      //Set have devices in unprocessed order cache to true
+      redisLite[order_id] = redisLite[order_id] || {};
+      redisLite[order_id].haveDevices = true;
+
+      // if (haveItems.length > 0) {
         //Search for user's order with no fraud score
-        let unprocessedOrders = await db.getUnprocessedOrder(message.user.id);
-        let unprocessedOrder = unprocessedOrders[0];
-        //Check if we have categories from this user's order
-        let haveCategories = await db.getItemsFromOrder(unprocessedOrder.id);
-        if (haveCategories.length > 0) {
-          //Send unprocessed order through to analysis
-          fraudAnalysis(unprocessedOrder.id);
-        }
+      if (redisLite[order_id].haveDevices && redisLite[order_id].haveCategories) {
+        //Send unprocessed order through to analysis
+        delete redisLite[order_id];
+        fraudAnalysis(order_id);
       } else {
         console.log('We do not have info from Inventory yet');
       }
 
     } else if (message.order_id) {//Message is from Inventory
       console.log('***Message is from Inventory***');
-      //Check if all categories are generated
-      let haveCategories = await Promise.all(message.items.map(item => db.getCategoryFraudRisk(item.category_id)));
-      // console.log('haveCategories', haveCategories);
-      //If the category does not exist
-      if (haveCategories.length === 0) {
-        //Generate categories
-        message.items.forEach(({category_name, category_id}) => {
-          console.log('category name', category_name);
-          console.log('category id', category_id);
-           (async () => await db.addNewCategory(category_name, category_id))();
-        })
-      }
 
+      //Generate categories
+      await Promise.all(message.items.map(({category_name, category_id}) => db.addNewCategory(category_name, category_id)))
       message.items.forEach(({category_name, category_id}) => {
         //Update item where order id 
          (async () => await db.updateCategoryId(category_id, message.order_id))();
       })
 
-      // //Check if we have devices for this user
-      let user_idResult = await db.getUserFromOrder(message.order_id);
-      // console.log('user id result', user_idResult);
-      let user_id = user_idResult[0].user_id;
-      // console.log('user id', user_id);
-      let haveDevices = await db.searchDevices(user_id);
-      if (haveDevices.length > 0) {
-        //Search for user's order with no fraud score
-        //Need to query the db for the user_id
-        // console.log('user id', user_id);
-        let unprocessedOrders = await db.getUnprocessedOrder(user_id);
-        // console.log('unprocessed orders', unprocessedOrders);
-        let unprocessedOrder = unprocessedOrders[0];
-        // console.log('unprocessed order', unprocessedOrder);
-        //Check if we have categories from this user's order
-        let haveCategories = await db.getItemsFromOrder(unprocessedOrder.id);
-        if (haveCategories.length > 0) {
+      //Set have categories in unprocessed order cache to true
+      redisLite[message.order_id] = redisLite[message.order_id] || {};
+      redisLite[message.order_id].haveCategories = true;
+
+      if (redisLite[message.order_id].haveDevices && redisLite[message.order_id].haveCategories) {
           //Send unprocessed order through to analysis
-          fraudAnalysis(unprocessedOrder.id);
-        }
+          delete redisLite[message.order_id];
+          fraudAnalysis(message.order_id);
       } else {
         console.log('Need info from User Activity');
       }
@@ -101,7 +84,7 @@ const haveAllOrderInfo = async message => {
         //Update order table
         await db.updateCB(message.order_id, moment(message.chargedback_at).format("YYYY-MM-DD HH:mm:ss"));
       } else {//Fraud analysis
-        console.log('***Analyzing for fraud***');
+        // console.log('***Analyzing for fraud***');
         //Save order to database
         await db.addNewOrder(
           message.order.order_id,
@@ -118,6 +101,7 @@ const haveAllOrderInfo = async message => {
         );
         //Save items to database
         await Promise.all(message.items.map(item => db.addNewItemFromOrder(item.item_id, message.order.order_id)))
+        
         //Send message to Users
         let usersParams = {
           MessageBody: JSON.stringify({
@@ -156,7 +140,7 @@ const fraudAnalysis = async order_id => {
   try {
     //Algorithm parameters
     const algWeight = 25;
-    const acceptableAOVStdDev = 2;
+    const acceptableAOVStdDev = 7;
     const acceptableNumOfDevices = 6;
     let fraud_score = 0;
 
@@ -172,8 +156,9 @@ const fraudAnalysis = async order_id => {
     //*** INFO FROM USER ***
     //Search for a user's devices
     let deviceResult = await db.searchDevices(user_id);
+    let uniqueDevices = _.uniq(uniqueDevices);
     // determine if # of devices is high
-    fraud_score += deviceResult.length < acceptableNumOfDevices ? 0 : algWeight;
+    fraud_score += uniqueDevices.length < acceptableNumOfDevices ? 0 : algWeight;
 
     //*** INFO FROM INVENTORY ***
     //Determine if order has items from high-risk categories
@@ -223,61 +208,28 @@ AWS.config.loadFromPath('./config.json');
 
 // SQS service objects
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'}); //For sending
-const sqsOrders = new AWS.SQS({apiVersion: '2012-11-05'});
-const sqsUsers = new AWS.SQS({apiVersion: '2012-11-05'});
-const sqsInventory = new AWS.SQS({apiVersion: '2012-11-05'});
+const sqsInbox = new AWS.SQS({apiVersion: '2012-11-05'});
 
 //Polling for messages
 
-  //Orders
-  const sqsConsumerOrders = Consumer.create({
-    queueUrl: ordersInbox,
+  //Inbox
+  const sqsConsumer = Consumer.create({
+    queueUrl: inbox,
     handleMessage: (message, done) => {
       haveAllOrderInfo(message.Body);
       done();
     },
-    sqs: sqsOrders
-  });
-
-  //Users
-  const sqsConsumerUsers = Consumer.create({
-    queueUrl: usersInbox,
-    handleMessage: (message, done) => {
-      haveAllOrderInfo(message.Body);
-      done();
-    },
-    sqs: sqsUsers
-  });
-
-  //Inventory
-  const sqsConsumerInventory = Consumer.create({
-    queueUrl: inventoryInbox,
-    handleMessage: (message, done) => {
-      haveAllOrderInfo(message.Body);
-      done();
-    },
-    sqs: sqsInventory
+    sqs: sqsInbox
   });
    
-  sqsConsumerOrders.on('error', (err) => {
+  sqsConsumer.on('error', (err) => {
     console.log(err.message);
   });
    
-  sqsConsumerOrders.start();
+  sqsConsumer.start();
 
-  sqsConsumerUsers.on('error', (err) => {
-    console.log(err.message);
-  });
-   
-  sqsConsumerUsers.start();
-
-  sqsConsumerInventory.on('error', (err) => {
-    console.log(err.message);
-  });
-   
-  sqsConsumerInventory.start();
 // ===========================================
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening on port 3000!');
 });
